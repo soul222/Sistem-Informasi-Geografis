87020ddcf4e045e2c9daae398c6e38a03b17d879
soul222 <soultanamirul222@gmail.com>
2026-01-17 23:09:43 +0700

    feat: Initialize JagaKota v1.0.1

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -2,7 +2,7 @@ export const ACCESS_TOKEN_KEY = 'accessToken';
 
 export const BASE_URL = 'https://citycare-api.dicoding.dev/v1';
 
-export const MAP_SERVICE_API_KEY = 'L1V7oYaAoswTHnKhMMJ8';
+export const MAP_SERVICE_API_KEY = 'SUh3wYOiVbBeZBbx2FjS';
 
 export const VAPID_PUBLIC_KEY =
   'BCCs2eonMI-6H2ctvFaWg-UYdDv387Vno_bzUzALpB442r2lCnsHmtrx8biyPi_E-1fSGABK_Qs_GlvPoJJqxbk';
